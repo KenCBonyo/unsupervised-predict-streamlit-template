688488a604700a26a4faca7025f18378e7f3a320
Treasuredife <treasuredife@gmail.com>
2022-07-28 18:13:05 +0100

    changed code to original code
    trouble shooting the error on the app

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -1,27 +1,27 @@
 """
-
+    
     Content-based filtering for item recommendation.
-
+    
     Author: Explore Data Science Academy.
-
+    
     Note:
     ---------------------------------------------------------------------
     Please follow the instructions provided within the README.md file
     located within the root of this repository for guidance on how to use
     this script correctly.
-
+    
     NB: You are required to extend this baseline algorithm to enable more
     efficient and accurate computation of recommendations.
-
+    
     !! You must not change the name and signature (arguments) of the
     prediction function, `content_model` !!
-
+    
     You must however change its contents (i.e. add your own content-based
     filtering algorithm), as well as altering/adding any other functions
     as part of your improvement.
-
+    
     ---------------------------------------------------------------------
-
+   
     Description: Provided within this file is a baseline content-based
     filtering algorithm for rating predictions on Movie data.
 
@@ -32,7 +32,6 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
@@ -42,20 +41,20 @@ movies.dropna(inplace=True)
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
-
+    
     Parameters
     ----------
     subset_size : int
         Number of movies to use within the algorithm.
-
+    
     Returns
     -------
     Pandas Dataframe
         Subset of movies selected for content-based filtering.
-
+    
     """
     # Split genre data into individual words.
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ', regex= True)
+    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
     # Subset of the data
     movies_subset = movies[:subset_size]
     return movies_subset
@@ -65,26 +64,26 @@ def data_preprocessing(subset_size):
 def content_model(movie_list,top_n=10):
     """Performs Content filtering based upon a list of movies supplied
        by the app user.
-
+    
     Parameters
     ----------
     movie_list : list (str)
         Favorite movies chosen by the app user.
     top_n : type
         Number of top recommendations to return to the user.
-
+    
     Returns
     -------
     list (str)
         Titles of the top-n movie recommendations to the user.
-
+    
     """
     # Initializing the empty list of recommended movies
     recommended_movies = []
     data = data_preprocessing(27000)
     # Instantiating and generating the count matrix
-    tf = TfidfVectorizer()
-    count_matrix = tf.fit_transform(data['keyWords'])
+    count_vec = CountVectorizer()
+    count_matrix = count_vec.fit_transform(data['keyWords'])
     indices = pd.Series(data['title'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     # Getting the index of the movie that matches the title
@@ -100,7 +99,7 @@ def content_model(movie_list,top_n=10):
     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
     # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
+    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
 
     # Store movie names
     recommended_movies = []
